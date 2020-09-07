7515d0d95ab8f18213ef86f850d87a007e5e0749
Nicolas Oga <2112.oga@gmail.com>
2020-09-07 16:06:49 -0300

    Setup ready

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Avantica Nu Order Coding Challenge',
+      template: 'src/index.html'
+    }),
+    new FriendlyErrorsWebpackPlugin(),
+    new ErrorOverlayPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts','.tsx', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    historyApiFallback: true
+  }
+};
