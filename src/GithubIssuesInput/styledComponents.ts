01a35a431b23c00f7a084818615b0a9230652756
Nicolas Oga <2112.oga@gmail.com>
2020-09-10 00:12:00 -0300

    Embelish

--- src/GithubIssuesInput/styledComponents.ts
+++ src/GithubIssuesInput/styledComponents.ts
@@ -17,7 +17,9 @@ const SuggestionsBox = styled.div`
   overflow: hidden;
   border: 1px solid lightgrey;
   box-sizing: border-box;
-  background: white;
+  background: #e0e2db;
+  border: 3px solid #191716;
+  border-radius: 8px;
 `;
 
 const SuggestionsBoxItem = styled.div`
@@ -26,20 +28,26 @@ const SuggestionsBoxItem = styled.div`
   cursor: pointer;
 
   &:not(:last-child) {
-    border-bottom: 1px solid lightgrey;
+    border-bottom: 1px solid #beb7a4;
   }
 
   &:hover,
   &.active {
-    background-color: tomato;
+    background-color: #e6af2e;
   }
 `;
 
 const Input = styled.input`
   width: 450px;
-  border: 1px solid: lightgrey;
+  background: #e0e2db;
+  border: 3px solid #191716;
+  border-radius: 8px;
   padding: 5px 8px;
   box-sizing: border-box;
+
+  &:focus {
+    outline: none !important;
+  }
 `;
 
 export { Root, SuggestionsBox, SuggestionsBoxItem, Input };
